3008f33b3bcc1935ea35675c7ef8d3e56e8445f9
Giovanecunhadias <giovanecunhadias@gmail.com>
2024-12-10 21:14:42 -0400

    feat: add reponsive navbar

---
+++ src/app/components/responsive_navbar.tsx
@@ -0,0 +1,42 @@
+
+"use client"
+import React, { useState } from "react"
+export default function Nav(){
+    const [isOpen, setIsOpen] = useState(false)
+    const buttonClasses = 'text-gray-100 font-bold text-sm px-2 py-1 border-2 border-gray-100 hover:bg-gray-100 hover:text-gray-800 rounded-lg trasition duration-300'
+    const buttons = (
+        <>
+            <button className={buttonClasses}>Blog</button>
+        </>
+    )
+    return(
+        <nav className="bg-gray-800 text-white fixed w-full">
+            <div className="max-w-7x1 mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between h-16">
+                    <div className="flex flex-row w-full justify-between">
+                        <div className="text-x1 font-bold">
+                            Brand Name
+                        </div>
+                        <div className="hidden md:block">
+                            <div className="flex ml-10 items-baseline space-x-2">
+                                Nav Links
+                            </div>
+                        </div>
+                    </div>
+                    <div className="md:hidden">
+                        <button onClick={()=>{setIsOpen(!isOpen)}} type="button" className="fill-gray-100">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-align-justify"><path d="M3 12h18"/><path d="M3 18h18"/><path d="M3 6h18"/></svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            {
+                isOpen && (
+                    <div className="flex flex-col gap-y-2 md:hidden px-4 sm:px-6 pb-2">
+                        {buttons}
+                    </div>
+                )
+            }
+        </nav>
+    )
+}
