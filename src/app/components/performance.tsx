d3925a8a30622c658264599f2af1a92c5e9a2521
Giovanedev <Giovanecunhadias>
2025-01-15 16:06:46 -0400

    update

--- src/app/components/performance.tsx
+++ src/app/components/performance.tsx
@@ -1,10 +1,11 @@
 import { CircleCheck, CircleX } from "lucide-react";
 import Image from 'next/image'
 
+
 export default function Performance() {
     return(
         <div className="flex flex-col gap-4 justify-center items-center text-center px-4 sm:px-6 lg:px-8 mt-10 sm:mt-20">
-                <div style={{ backgroundColor: 'rgba(95, 194, 238, 0.5)'}} className="rounded-full w-28 text-center">
+                <div style={{ backgroundColor: 'rgba(95, 194, 238, 0.5)'}} className="rounded-full w-30 text-center">
                     <span className="font-faktum text-[#5fc2ee] text-sm sm:text-base">PERFORMANCE</span>
                 </div>
                 <h2 className="font-faktum text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
@@ -17,8 +18,14 @@ export default function Performance() {
                         <div className="bg-white rounded-xl border-2 p-4 flex flex-col items-center">
                             <p className='font-faktum text-2xl sm:text-3xl text-[#1f073b]'>Dashboard</p>
                             <p className='font-faktum text-2xl sm:text-3xl text-[#1f073b]'>em tempo real.</p>
+                            <Image src="/dashboard.png" alt="dashboard" width={200} height={200}/>
                         </div>
-                        
+                        <div className="bg-white rounded-xl border-2 p-4 flex flex-col items-center">
+                            <Image src="years.svg" width={200} height={200} alt="icon"/>
+                            <p className="font-faktum font-bold text-[#1f073b]">Importe suas</p>
+                            <p className="font-faktum font-bold text-[#1f073b]">vendas antigas.</p>
+                            <Image src="spedy86.svg" width={200} height={200} alt="icon"/>
+                        </div>
                     </div>
 
                     <div className='text-left space-y-4'>
@@ -47,9 +54,43 @@ export default function Performance() {
                         </div>
                         
                     </div>
-                    <div className='text-left'>
-                        <p className='font-faktum text-2xl sm:text-3xl text-[#1f073b]'>Divisão entre coprodutores.</p>
-                        <p className='font-faktum text-2xl sm:text-3xl text-[#1f073b]'>Split de suas notas.</p>
+                    <div className='text-left space-y-4'>
+                        
+                        <div className="bg-white rounded-xl border-2 p-4 flex flex-col items-center relative">
+                            <p className='font-faktum text-2xl sm:text-3xl text-[#1f073b]'>Divisão entre </p>
+                            <p className='font-faktum text-2xl sm:text-3xl text-[#1f073b]'>coprodutores.</p>
+                            
+                            <div className="relative w-[300px] h-[200px] my-4">
+                                {/* SVG da divisão */}
+                                <Image src="divisao.svg" alt="icon" width={100} height={100} className="mx-auto"/>
+                                
+                                {/* Foto do coprodutor 1 - esquerda */}
+                                <div className="absolute left-20 bottom-0">
+                                <img 
+                                    src="https://framerusercontent.com/images/UUzCrpytoCj3VdSEkdlYSns86U.png" 
+                                    alt="Coprodutor 1" 
+                                    className="w-12 h-12 rounded-full object-cover border-2 border-white"
+                                />
+                                </div>
+                                
+                                {/* Foto do coprodutor 2 - direita */}
+                                <div className="absolute right-20 bottom-0">
+                                <img 
+                                    src="https://framerusercontent.com/images/LCpfHb5KZMbMEqxj5CVnuj99EaA.png" 
+                                    alt="Coprodutor 2" 
+                                    className="w-12 h-12 rounded-full object-cover border-2 border-white"
+                                />
+                                </div>
+                            </div>
+
+                            
+                        </div>
+                        <div className="bg-white rounded-xl border-2 p-4 flex flex-col items-center">
+                            <Image src="split.svg" alt="icon" width={100} height={100}/>
+                            <p className='font-faktum text-2xl sm:text-3xl text-[#1f073b]'>Split fiscal de suas notas.</p>
+                        </div>
+
+                        
                     </div>
                 </div>
                 {/*FALTANDO IMAGENS */}
