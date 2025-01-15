289be7ceba4219f17b4ba99091ed4afc4662b717
Giovanedev <Giovanecunhadias>
2025-01-15 14:49:24 -0400

    update

--- src/app/components/performance.tsx
+++ src/app/components/performance.tsx
@@ -1,4 +1,5 @@
 import { CircleCheck, CircleX } from "lucide-react";
+import Image from 'next/image'
 
 export default function Performance() {
     return(
@@ -11,24 +12,40 @@ export default function Performance() {
                 </h2>
                 <div className='grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
                     {/*DASHBOARD EM TEMPO REAL */}
-                    <div className='text-left'>
-                        <p className='font-faktum text-2xl sm:text-3xl text-[#1f073b]'>Dashboard</p>
-                        <p className='font-faktum text-2xl sm:text-3xl text-[#1f073b]'>em tempo real.</p>
+                    <div className='text-left space-y-4'>
+
+                        <div className="bg-white rounded-xl border-2 p-4 flex flex-col items-center">
+                            <p className='font-faktum text-2xl sm:text-3xl text-[#1f073b]'>Dashboard</p>
+                            <p className='font-faktum text-2xl sm:text-3xl text-[#1f073b]'>em tempo real.</p>
+                        </div>
+                        
                     </div>
 
-                    <div className='text-left'>
-                        <p className='font-faktum text-xl sm:text-2xl text-[#1f073b]'>Fechamento</p>
-                        <p className='font-faktum text-xl sm:text-2xl text-[#1f073b]'>mensal em dia.</p>
-                        <p className='font-faktum text-lg sm:text-xl text-[#1f073b]'>Sem precisar enviar</p>
-                        <p className='font-faktum text-lg sm:text-xl text-[#1f073b]'>o XML ao contador.</p>
+                    <div className='text-left space-y-4'>
+                        <div className="bg-white rounded-xl border-2 p-4 flex flex-col items-center">
+                            <p className='font-faktum text-xl sm:text-2xl text-[#1f073b]'>Fechamento</p>
+                            <p className='font-faktum text-xl sm:text-2xl text-[#1f073b]'>mensal em dia.</p>
+                            
+                            <Image src="fechamento_mensal.svg"  width={100} height={100} alt="icon"/>
+                            
+                            
+                            <p className='font-faktum text-lg sm:text-xl text-[#1f073b]'>Sem precisar enviar</p>
+                            <p className='font-faktum text-lg sm:text-xl text-[#1f073b]'>o XML ao contador.</p>
+                        </div>
+                        <div className="bg-white rounded-xl border-2 p-4 flex flex-col items-center">
+                            <Image src="suporte_operacao_dp.svg" alt="operação" width={100} height={100}/>
+                            <p className='font-faktum text-lg sm:text-xl text-[#1f073b] mt-4'>Suporte total para</p>
+                            <p className='font-faktum text-lg sm:text-xl text-[#1f073b]'>sua operação de</p>
+                            <p className='font-faktum text-[#d93d8d] text-3xl sm:text-4xl'>Dropshipping</p>
+                        </div>
                         
-                        <p className='font-faktum text-lg sm:text-xl text-[#1f073b] mt-4'>Suporte total para</p>
-                        <p className='font-faktum text-lg sm:text-xl text-[#1f073b]'>sua operação de</p>
-                        <p className='font-faktum text-[#d93d8d] text-3xl sm:text-4xl'>Dropshipping</p>
-                        <p className='font-faktum text-2xl sm:text-3xl text-[#d93d8d] flex items-center'><CircleCheck color='#ffff' fill='#01b169' size={24} className="mr-2"/>Cancelamento ou</p>
-                        <p className='font-faktum text-2xl sm:text-3xl text-[#d93d8d] flex items-center'><CircleX fill='#d62e85' color='#ffff' size={24} className="mr-2"/>devolução autmático:</p>
-                        <p className='font-faktum text-xl sm:text-2xl text-[#1f073b] flex items-center'><CircleCheck color='#ffff' fill='#01b169' size={24} className="mr-2"/>Quando uma venda é reembolsada o</p>
-                        <p className='font-faktum text-xl sm:text-2xl text-[#1f073b] flex items-center'><CircleCheck color='#ffff' fill='#01b169' size={24} className="mr-2"/>Ameii Cancela!</p>
+                        <div className="bg-white rounded-xl border-2 p-4 flex flex-col items-center">
+                            <p className='font-faktum text-2xl sm:text-3xl text-[#d93d8d] flex items-center'><CircleCheck color='#ffff' fill='#01b169' size={24} className="mr-2"/>Cancelamento ou</p>
+                            <p className='font-faktum text-2xl sm:text-3xl text-[#d93d8d] flex items-center'><CircleX fill='#d62e85' color='#ffff' size={24} className="mr-2"/>devolução autmático:</p>
+                            <p className='font-faktum text-xl sm:text-2xl text-[#1f073b] flex items-center'><CircleCheck color='#ffff' fill='#01b169' size={24} className="mr-2"/>Quando uma venda é reembolsada o</p>
+                            <p className='font-faktum text-xl sm:text-2xl text-[#1f073b] flex items-center'><CircleCheck color='#ffff' fill='#01b169' size={24} className="mr-2"/>Ameii Cancela!</p>
+                        </div>
+                        
                     </div>
                     <div className='text-left'>
                         <p className='font-faktum text-2xl sm:text-3xl text-[#1f073b]'>Divisão entre coprodutores.</p>
